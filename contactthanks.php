711842d08fd22296a3e0d51dcfbd3a396df9b449
lawlere <emily.lawler@colorado.edu>
2013-11-07 21:58:21 -0800

    copy updates

---
+++ contactthanks.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title>Save Home Heat</title>
+	
+	<link rel="stylesheet" type="text/css" href="shh.css" />
+</head>
+
+<body>
+
+	<div id="page-wrap">
+
+		<img src="images/title.gif" alt="Thank you form" />
+			
+			
+		<br /><br />
+		
+			
+		<h1>Your message has been sent!</h1><br />
+		
+		<p><a href="contact.html">Back to Contact Form</a></p>
+	
+	</div>
+	
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script>
+	<script type="text/javascript">
+	_uacct = "UA-68528-29";
+	urchinTracker();
+	</script>
+
+</body>
+
+</html>
